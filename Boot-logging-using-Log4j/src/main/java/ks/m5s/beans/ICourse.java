26dc950a02e74e3d5fcfd929d1be671b177c2565
kate-2022 <101457059+kate-2022@users.noreply.github.com>
2023-04-16 13:21:16 +0200

    Add files via upload

---
+++ Boot-logging-using-Log4j/src/main/java/ks/m5s/beans/ICourse.java
@@ -0,0 +1,10 @@
+package ks.m5s.beans;
+
+// Java, DotNet, UI
+public interface ICourse {
+	
+	public String courseContent();
+
+	public float price();
+
+}
---
+++ Boot-logging-using-Log4j/src/test/java/ks/m5s/BootLoggingUsingLog4jApplicationTests.java
@@ -0,0 +1,13 @@
+package ks.m5s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootLoggingUsingLog4jApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
